4a679889ccee44da432ee6bb2fecba20e466b7d8
Jack Frost <j4qfrost@gmail.com>
2019-07-26 04:27:18 -0700

    source

---
+++ factorial.py
@@ -0,0 +1,5 @@
+def factorial(n):
+	result = 1
+	for i in range(n):
+		result *= i
+	return result
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='factorial',
+   version='1.0',
+   description='',
+   author='',
+   author_email='',
+   packages=['factorial'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
