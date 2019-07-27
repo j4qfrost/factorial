ff1c0f67d7df609da35bfa2c8d89f59bcb42e032
Jack Frost <j4qfrost@gmail.com>
2019-07-26 17:58:06 -0700

    fix import

---
+++ factorial/factorial.py
@@ -0,0 +1,5 @@
+def factorial(n):
+	result = 1
+	for i in range(n):
+		result *= i + 1
+	return result
--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 
 setup(
    name='factorial',
+   url='https://github.com/j4qfrost/factorial.git',
    version='1.0',
    description='',
    author='',
    author_email='',
+   modules=['factorial'],
    install_requires=[], #external packages as dependencies
-)
+   zip_safe=False
+ )
