e09019a353a8d2a284aad290a29f56def201a05c
Jack Frost <j4qfrost@gmail.com>
2019-08-24 01:56:55 -0700

    change structure

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
    description='',
    author='',
    author_email='',
-   modules=['.'],
+   modules=['factorial'],
    install_requires=[], #external packages as dependencies
    zip_safe=False
  )
