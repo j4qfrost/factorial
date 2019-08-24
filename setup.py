7cc2efa5ac7ecdb507558810ace9d26ce0799d23
Jack Frost <j4qfrost@gmail.com>
2019-08-24 01:53:49 -0700

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='factorial',
    url='https://github.com/j4qfrost/factorial.git',
-   version='1.0',
+   version='0.1.0',
    description='',
    author='',
    author_email='',
